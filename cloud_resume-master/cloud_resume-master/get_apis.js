0c2534322c36ead0b8a6ae442dcc2c9c8cac804b
salihsedat <66579071+salihsedat@users.noreply.github.com>
2022-05-04 12:39:29 -0400

    Update get_apis.js

--- cloud_resume-master/cloud_resume-master/get_apis.js
+++ cloud_resume-master/cloud_resume-master/get_apis.js
@@ -1,5 +1,4 @@
-const tempUrl = 'https://weatherpi-api.tomasko.eu'
-const tempElement = document.getElementById('temp');
+
 const countUrl = 'https://emil3mqc5f.execute-api.eu-central-1.amazonaws.com/prod/counter'
 const countElement = document.getElementById('count');
 
@@ -10,17 +9,5 @@ function updateVisitCount() {
         .then(res => res.json())
         .then(res => {
         countElement.innerHTML = res.visits;
-    });
-}
-
-updateTemp();
-
-function updateTemp() {
-    fetch(tempUrl)
-        .then(res => res.json())
-        .then(res => {
-// jsonpath doesn't work. quick and dirty fix now, proper fix TBD
-//        tempElement.innerHTML = res.data.result[?(@.metric.job=='weatherpi')].value[1];
-        tempElement.innerHTML = res.data.result[0].value[1];
     });
 }
